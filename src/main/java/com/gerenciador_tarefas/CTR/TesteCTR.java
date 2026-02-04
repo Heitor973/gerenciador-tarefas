63da76e8bb94928d80ab9dbe88727f80494b8970
Heitor973 <heitorquatrini@gmail.com>
2026-02-04 19:57:23 -0300

    Update TesteCTR.java

--- src/main/java/com/gerenciador_tarefas/CTR/TesteCTR.java
+++ src/main/java/com/gerenciador_tarefas/CTR/TesteCTR.java
@@ -1,6 +1,7 @@
 package com.gerenciador_tarefas.CTR;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,5 +10,9 @@ public class TesteCTR {
     @GetMapping("/teste-api")
     private String teste(){
         return "Susesso";
+    }
+    @GetMapping("/teste-api-ben-vindo")
+    public String TesteBemVindo(@RequestParam(name ="nome") String nome){
+return "Olá "+nome+", Bem-vindo" ;
     }
 }
